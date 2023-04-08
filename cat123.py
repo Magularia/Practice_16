50c37c15662b779b86f5fc8fe5a8983a08bcbe04
Magularia <you@example.com>
2023-04-08 16:53:00 +0300

    Initial commit

---
+++ cat123.py
@@ -0,0 +1,13 @@
+from cat import Cat
+
+cat1 = Cat("Машка", "3", "девочка")
+
+print("Имя питомца1:", cat1.name)
+print("Возраст питомца1:", cat1.age)
+print("Пол питомца1:", cat1.gender)
+
+cat2 = Cat("Барсик", "1", "мальчик")
+
+print("Имя питомца2:", cat2.name)
+print("Возраст питомца2:", cat2.age)
+print("Пол питомца2:", cat2.gender)
